669574e1f276db3a049b215e02fade1cad7b2c61
Leonardo <leocairos@gmail.com>
2021-06-27 18:11:22 -0300

    fix ensureauthentica to use token

--- src/shared/infra/http/middlewares/ensureAuthenticate.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticate.ts
@@ -16,7 +16,7 @@ export async function ensureAuthenticated(
   next: NextFunction,
 ) {
   const authHeader = request.headers.authorization;
-  const usersTokensRepository = new UsersTokensRepository();
+  // const usersTokensRepository = new UsersTokensRepository();
 
   if (!authHeader) throw new AppError('Token missing', 401);
   // Bearer 49ewfw94f971w971we9fwe
@@ -24,17 +24,7 @@ export async function ensureAuthenticated(
   // [1] = 49ewfw94f971w971we9fwe
   const [, token] = authHeader.split(' ');
   try {
-    const { sub: user_id } = verify(
-      token,
-      auth.secret_refresh_token,
-    ) as IPayload;
-
-    const user = await usersTokensRepository.findByUserIdAndRefreshToken(
-      user_id,
-      token,
-    );
-
-    if (!user) throw new AppError('User does not exist', 401);
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
     request.user = { id: user_id };
     next();
