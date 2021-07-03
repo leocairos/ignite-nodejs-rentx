b451bd4ff172ecfb10387d4e3b4bd55f2f319210
Leonardo <leocairos@gmail.com>
2021-07-03 11:27:46 -0300

    ratelimiter and sentry implementation

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,17 +6,30 @@ import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
 import '@shared/container';
-
 import upload from '@config/upload';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import { AppError } from '@shared/errors/AppError';
 import { router } from '@shared/infra/http/routes';
 import createConnection from '@shared/infra/typeorm';
 
 import swaggerFile from '../../../swagger.json';
+import { rateLimiter } from './middlewares/rateLimiter';
 
 createConnection();
 
 const app = express();
+
+app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
 
 app.use(cors());
 app.use(express.json());
@@ -27,12 +40,27 @@ const options = {
   // customfavIcon: '/assets/favicon.ico',
 };
 
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile, options));
 
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
+
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      // Capture all 429 and 500 errors
+      if (error.status === 429 || error.status === 500) {
+        return true;
+      }
+      return false;
+    },
+  }),
+);
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
